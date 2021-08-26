53af3a94a1dafeb25cb035067a44f826860dd8ef
NovemOfHidden <70879164+NovemOfHidden@users.noreply.github.com>
2021-08-26 10:19:42 +0700

    Auth

--- resources/views/profile.blade.php
+++
@@ -1,6 +0,0 @@
-<h2>hello {{session('user')}}</h2>
-
-@if(session('user')=='admin')
-     <a href="./admin">admin</a><br/>
-@endif
-<a href="./logout">logout</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Menber;
 use App\Http\Middleware\AdminRole;
 use App\Http\Controllers\Auth1;
@@ -16,38 +17,41 @@ use App\Http\Controllers\Auth1;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
-Route::post("user",[Auth1::class,'auth']);
-Route::get('/login', function () {
-    return view('login');
-})->name("login");
+// Route::get('/', function () {
+//     return view('login');
+// });
+// // Route::post("user",[Auth1::class,'auth']);
+// // Route::get('/login', function () {
+// //     return view('login');
+// // })->name("login");
 
-Route::post("register",[Auth1::class,'register']);
-Route::get('/register', function () {
-    return view('register');
-})->name("register");
+// // Route::post("register",[Auth1::class,'register']);
+// // Route::get('/register', function () {
+// //     return view('register');
+// // })->name("register");
 
-Route::get('admin',function(){
-    return view("Admin.admin");
-})->middleware('AdminRole');
+// Route::get('admin',function(){
+//     return view("Admin.admin");
+// })->middleware('AdminRole');
 
-Route::get('profile', function () {
-    return view('profile');
-})->name("profile");
-Route::get('logout',function(){
-    if(session()->has('user')){
-        session()->pull('user');
-        return redirect('login');
-    }
-    else{
-        return redirect('login');
-    }
-});
-Route::middleware(['localization'])->group(function () {
-    Route::get('/welcome', function () {
-        return view('Admin.welcome');
-    });
-    Route::get('change-language/{language}',[Auth1::class,'changeLanguage'])->name('change-language');
-});
+// Route::get('profile', function () {
+//     return view('profile');
+// })->name("profile");
+// Route::get('logout',function(){
+//     if(session()->has('user')){
+//         session()->pull('user');
+//         return redirect('login');
+//     }
+//     else{
+//         return redirect('login');
+//     }
+// });
+// Route::middleware(['localization'])->group(function () {
+//     Route::get('/welcome', function () {
+//         return view('Admin.welcome');
+//     });
+//     Route::get('change-language/{language}',[Auth1::class,'changeLanguage'])->name('change-language');
+// });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
